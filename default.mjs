a651560fcba383a4a82898a253df589b0a6c8c76
eduardo demuner <eduardood15@hotmail.com>
2022-08-14 16:45:32 -0300

    writing new html from the previous using automated template replacing method

--- default.mjs
+++ default.mjs
@@ -1,5 +1,14 @@
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 
 let template = await readFile(new URL('./template.html', import.meta.url), 'utf-8')
 
-console.log(template)
+const data = {
+    title:'My new file',
+    body: 'I wrote this file to disk using node'
+}
+
+for (const [key, val] of Object.entries(data)){
+    template = template.replace(`{${key}}`, val)
+}
+
+await writeFile(new URL('./new.html', import.meta.url), template)
